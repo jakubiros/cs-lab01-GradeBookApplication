75cee0ae66ab781e583a1757e8d157670e7c3323
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2023-03-31 22:59:36 +0200

    Override GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Math;
 
 namespace GradeBook.GradeBooks
 {
@@ -11,6 +12,45 @@ namespace GradeBook.GradeBooks
         public  RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            int totalStudents = Students.Count;
+            int top20Percent = (int)Math.Round(totalStudents * 0.2);
+            int top40Percent = (int)Math.Round(totalStudents * 0.4);
+            int top60Percent = (int)Math.Round(totalStudents * 0.6);
+            int top80Percent = (int)Math.Round(totalStudents * 0.8);
+            List<Student> sortedStudents = Students.OrderByDescending(s => s.AverageGrade).ToList();
+            List<double> sortedGrades=new List<double>();
+            foreach (Student student in sortedStudents)
+            {
+                sortedGrades.Add(student.AverageGrade);
+            }
+            int? index = null;
+            ind:
+            index = sortedGrades.FindIndex(s => s == averageGrade);
+
+            if (index < 0)
+            {
+                sortedGrades.Add(averageGrade);
+                sortedGrades = sortedGrades.OrderByDescending(s => s).ToList();
+                goto ind;
+            }
+
+            if (index < top20Percent)
+                return 'A';
+            else if (index < top40Percent)
+                return 'B';
+            else if (index < top60Percent)
+                return 'C';
+            else if (index < top80Percent)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
